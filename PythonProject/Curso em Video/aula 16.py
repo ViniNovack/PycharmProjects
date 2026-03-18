5a5ad4a467781548f160f1d50a90212ad019f6f7
ViniNovack <vininomn@gmail.com>
2026-03-17 22:12:07 -0300

    Atualização 3.8

---
+++ PythonProject/Curso em Video/aula 16.py
@@ -0,0 +1,22 @@
+#EXEMPLO 1
+# lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#
+# # for cont in range(0, len(lanche)):
+# #     print(lanche[cont])
+#
+# for pos, comida in enumerate(lanche):
+#     print(f'Eu vou comer {comida} na posição {pos + 1}')
+#
+# print('Comi muito')
+
+#EXEMPLO 2
+# lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+#
+# print(sorted(lanche))
+# print(lanche)
+
+#EXEMPLO 3
+# a = (2, 5, 4)
+# b = (5, 8, 1, 2)
+# c = b + a
+# print(c.count(5))
