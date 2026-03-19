67ebba1982322b8b3a4d7c277c34262e50510288
ViniNovack <vininomn@gmail.com>
2026-03-19 15:45:45 -0300

    Atualização 1.1.3

---
+++ PythonProject/Faculdade/Aula Semana 4.py
@@ -0,0 +1,29 @@
+#ATIVIDADE 1
+# t = float(input('Digite uma temperatura: \n'))
+# if t > 25:
+#     print('Esta quente 😍')
+# elif 18 <= t <= 24:
+#     print('Esta amena')
+# else:
+#     print('Esta frio 🥶🥶🥶')
+
+#ATIVIDADE 2
+# n = int(input('Digite um numero: \n'))
+# if n % 2 == 0:
+#     print(f'O numero {n} é PAR')
+# else:
+#     print(f'O numero {n} é IMPAR')
+
+#ATIVIDADE 3
+# idade = int(input('Idade: \n'))
+# ingresso = str(input('Voce tem ingreço? (s/n) \n'))
+#
+# if ingresso == 's':
+#     ingresso = True
+# elif ingresso == 'n':
+#     ingresso = False
+#
+# if idade >= 18 and ingresso == True:
+#     print('bom filme!')
+# else:
+#     print('barrado')
---
+++ PythonProject/Faculdade/Exercicios Semana 4.py
@@ -0,0 +1,63 @@
+#EXERCICIO 1
+# print('Voce esta perdido na floresta e precisa escolher entre caminhos!!!! \n')
+# while True:
+#     c = str(input('Voce pode ir para a DIREITA ou para ESQUERDA \n'))
+#     if c == 'DIREITA':
+#         print('PARABENS!!!!!!!!, voce achou uma montanha')
+#         s = str(input('Voce pode SUBIR ou VOLTAR \n'))
+#         if s == 'SUBIR':
+#             print('PARABENS!!!!!!!!, Voce achou um TESOURO')
+#             break
+#         elif s == 'VOLTAR':
+#             print('Voce continua perdido')
+#             continue
+#
+#     if c == 'ESQUERDA':
+#         print('PARABENS!!!!!!!, voce achou um RIO')
+#         a = str(input('Voce pode ATRAVESSAR O RIO ou VOLTAR \n'))
+#         if a == 'ATRAVESSAR':
+#             print('Voce achou uma vila segura')
+#             break
+#         elif a =='VOLTAR':
+#             print('Voce continua perdido')
+#             continue
+from idlelib.debugobj import ClassTreeItem
+
+#EXERCICIO 2
+# n = int(input('Digite um número: \n'))
+# if (10 < n) and (n < 50):
+#     print(f'O número {n} esta no intervalo')
+# if 10 > n:
+#     print(f'O número {n} é MENOR que 10')
+# if n > 50:
+#     print(f'O número {n} é MAIOR que 50')
+
+#EXERCICIO 3
+# a = int(input('Digite um ano: \n'))
+# if a % 4 == 0 and a % 100 != 0 or a % 400 == 0:
+#     print(f'O ano {a} é BISSEXTO')
+# else:
+#     print(f'O ano {a} NÃO é BISSXTO')
+
+#EXERCICIO 4
+# u = str(input('Escreva seu usuario: \n'))
+# if u == 'convidado':
+#     print('Acesso restrito')
+# else:
+#     s = int(input('Digite sua senha: \n'))
+#     if u == 'admin' and s == 1234:
+#         print('Acesso permitido')
+#     else:
+#         print('BLOQUEADO')
+
+#EXERCICIO 5
+# x = int(input('Digite a condenada X \n'))
+# y = int(input('Digite a condenada Y \n'))
+# if x > 0 and y > 0 and x < 10 and y < 10:
+#     print(f'A região {x,y} está Dentro do quadrado')
+# elif x == 0 and 0 <= y <= 10 or 0 <= y <= 10 and x == 10 or 0 <= x <= 10 and y == 0 or y ==0 and 0 <= x <= 10:
+#     print(f'A região {x,y} está Na fronteira')
+# else:
+#     print(f'A região {x,y} está Fora do quadrado')
+
+#EXERCICO 6 (DESAFIO)
