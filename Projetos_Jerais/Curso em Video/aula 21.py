6a6f81cdb800d3aa7970e26077fc1359b861a142
ViniNovack <vininomn@gmail.com>
2026-04-08 18:07:26 -0300

    Atualização Curso em Vídeo aula 21

--- Projetos_Jerais/Curso em Video/aula 21.py
+++ Projetos_Jerais/Curso em Video/aula 21.py
@@ -1,1 +1,47 @@
 # EXEMPLO 1
+# def função():
+#     global n1
+#     n1 = 4
+#     print(f'N1 dentro vale {n1}')
+
+
+# n1 = 2
+# função()
+# print(f'N1 fora vale {n1}')
+
+# EXEMPLO 2
+# def somar(a=0, b=0, c=0):
+#     s = a + b + c
+#     return s
+
+# r1 = somar(3, 2, 5)
+# r2 = somar(1, 7)
+# r3 = somar(4)
+# print(f'Meus cálculos deram {r1}, {r2} e {r3}!')
+
+# EXEMPLO 3
+# def fatorial(num = 1):
+#     f = 1
+#     for c in range(num, 0, -1):
+#         f *= c
+#     return f
+
+
+# x = int(input('Digite um número para calcular seu fatorial: \n'))
+# print(f'{fatorial(x)}')
+
+# EXEMPLO 4
+# def par(n=0):
+#     if n % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+
+# num = int(input('Digite um número: \n'))
+# if par(num):
+#     print('É PAR!')
+# else:
+#     print('É IMPAR!')
+
+# EXERCICIO 101
