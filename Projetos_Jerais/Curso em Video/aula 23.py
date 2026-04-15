5c7c661c308a8425770a67996709e73dcc7a791f
ViniNovack <vininomn@gmail.com>
2026-04-15 17:11:07 -0300

    Atualização exercicio 113

--- Projetos_Jerais/Curso em Video/aula 23.py
+++ Projetos_Jerais/Curso em Video/aula 23.py
@@ -1,11 +1,40 @@
 # EXEMPLO 1
-try:
-    a = int(input('Numerador: '))
-    b = int(input('Denominador: '))
-    r = a / b
-except:
-    print(f'Problema encontrado.')
-else:
-    print(f'O resultado é {r:.1f}')
-finally:
-    print('Volte sempre! Muito obrigado!')
+# try:
+#     a = int(input('Numerador: '))
+#     b = int(input('Denominador: '))
+#     r = a / b
+# except (ValueError, TypeError):
+#     print('Tivemos um problema com os tipos de dados que você digitou.')
+# except (ZeroDivisionError):
+#     print('Não é possível dividir um número por zero!')
+# except KeyboardInterrupt:
+#     print('O usuário preferiu não informar os dados!')
+# except Exception as erro:
+#     print(f'O erro encontrado foi {erro.__cause__}')
+# else:
+#     print(f'O resultado é {r:.1f}')
+# finally:
+#     print('Volte sempre! Muito obrigado!')
+
+# EXERCICIO 113
+f = 0
+while True:
+    i = str(input('Digite um Inteiro: '))
+    try:
+        i = int(i)
+        while True:
+            f = str(input('Digite um Real: '))
+            try:
+                f = float(f)
+                break
+            except KeyboardInterrupt:
+                print('O usuario preferiu não digitar o numero Real')
+                break
+            except:
+                print('ERRO: por favor, digite um número real válido.')
+                continue
+        break
+    except:
+        print('ERRO: por favor, digite um número inteiro válido.')
+        continue
+print(f'O valor inteiro digitado foi {i} e o real foi {f}')
